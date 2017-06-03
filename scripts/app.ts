fb74fa54dc241c3336ceabffa268685e83c318fb
Otto Streifel <ottost@microsoft.com>
2017-06-02 21:23:08 -0700

    include img and click properties

--- scripts/app.ts
+++ scripts/app.ts
@@ -1,14 +1,22 @@
 ///<reference types="vss-web-extension-sdk" />
+import * as Q from "q";
 
 export interface IContributedQueryResultColumnValue {
     title: string;
+    image: string;
+    onClick: () => IPromise<void>;
 }
 var actionProvider =  {
     getValue: (wiData: {[id: number]: {[refName: string]: any}}) => {
         const results: {[id: number]: IContributedQueryResultColumnValue} = {};
         for (const id in wiData) {
             results[id] = {
-                title: `Hello ${id} from extension`
+                title: `Hello ${id} from extension`,
+                image: "http://batzner.com/blog/wp-content/uploads/2011/11/bed-bug.jpg",
+                onClick: () => {
+                    alert("Hello from alert");
+                    return Q();
+                }
             };
         }
         return results;
